12833119722f1d41b39149ade884fbfa07b6ab98
kumaramit-bhabra <Loadrunner_1>
2019-12-30 16:54:47 +0000

    First Changes

---
+++ cypress/integration/examples/index.js
@@ -0,0 +1,8 @@
+/// <reference types="Cypress" />
+describe("First test case is Cypress", ()=>{
+    it("First Test case",()=>{
+        cy.visit("https://www.adactin.com/HotelApp/");
+    })
+
+    
+})
